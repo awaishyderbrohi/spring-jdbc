07b341551f16ef43d851923caebeee4aad624c33
awaishyder <awaishyderbr@gmail.com>
2024-08-03 16:04:20 -0700

    config.xml modified!

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -1,11 +1,15 @@
 package com.springcore;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
