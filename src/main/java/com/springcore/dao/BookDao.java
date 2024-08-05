b643fcf9cd80e6008a6e9bdf4792ec5353314eb1
Awais Hyder <awaishyderbr@gmail.com>
2024-08-05 11:36:50 -0700

    Update BookDao.java

--- src/main/java/com/springcore/dao/BookDao.java
+++ src/main/java/com/springcore/dao/BookDao.java
@@ -6,7 +6,6 @@ import com.springcore.entity.Book;
 import java.util.List;
 
 public interface BookDao {
-
     public String create(Book book);
     public String update(Book book);
     public String delete(Book book);
